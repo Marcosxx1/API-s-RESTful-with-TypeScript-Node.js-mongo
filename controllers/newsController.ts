8b167fc9d338ec84d2abfbf206310a9a83182349
Marcos Alex <marcosxx1.ma@gmail.com>
2023-03-04 11:18:57 -0400

    Implementado controllers / get

--- controllers/newsController.ts
+++ controllers/newsController.ts
@@ -1,11 +1,24 @@
 import { Request, Response } from 'express';
 import newsServices from '../services/newsServices';
+import * as HttpStatus from "http-status";
+
+
+
+
 
 class newsController {
-    get(req: Express.Request, res: Express.Response) { }
-    getById(req: Express.Request, res: Express.Response) { }
-    update(req: Express.Request, res: Express.Response) { }
-    delete(req: Express.Request, res: Express.Response) { }
+    sendResponse = function (res, statusCode, data) {
+        res.status(statusCode).json({ reult: data });
+    }
+
+    get(req: Request, res: Response) {
+        newsServices.get()
+            .then(news => this.sendResponse(res, HttpStatus.OK, news))
+            .catch(error => console.error.bind(console, `Error ${error}`))
+    }
+    getById(req: Request, res: Response) { }
+    update(req: Request, res: Response) { }
+    delete(req: Request, res: Response) { }
 }
 
 export default new newsController();
